5d381d3189a2491971da2d844a5f9eb2e563f337
unknown <quentinandrieumail@gmail.com>
2014-05-23 14:17:51 +0200

    Merge temporaire Testting

--- src/ImieNetwork/SiteBundle/Controller/AdministrationController.php
+++ src/ImieNetwork/SiteBundle/Controller/AdministrationController.php
@@ -4,12 +4,82 @@ namespace ImieNetwork\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ImieNetwork\SiteBundle\Entity\Utilisateur;
+use ImieNetwork\SiteBundle\Form\UtilisateurType;
 
-class AdministrationController extends Controller {
+class AdministrationController extends Controller
+{
+    public function indexUtilisateurAction()
+    {
+        $em = $this->getDoctrine()->getManager();
 
-    public function indexAction() {
-        return $this->render('ImieNetworkSiteBundle:Administration:index.html.twig');
+        $entities = $em->getRepository('ImieNetworkSiteBundle:Utilisateur')->findAll();
+        
+        return $this->render("@Administration\Utilisateur\index.html.twig",array(
+            'entities' => $entities,
+        ));
+    }  
+    public function newUtilisateurAction()
+    {
+        $entity = new Utilisateur();
+        $form   = $this->createCreateFormUtilisateur($entity);
+
+        return $this->render('@Administration\Utilisateur\new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
     }
+    
+    public function editUtilisateurAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
 
+        $entity = $em->getRepository('ImieNetworkSiteBundle:Utilisateur')->find($id);
 
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Utilisateur entity.');
+        }
+
+        $editForm = $this->createEditFormUtilisateur($entity);
+        $deleteForm = $this->createDeleteFormUtilisateur($id);
+
+        return $this->render("@Administration\Utilisateur\edit.html.twig", array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    
+    private function createCreateFormUtilisateur(Utilisateur $entity)
+    {
+        $form = $this->createForm(new UtilisateurType(), $entity, array(
+            'action' => $this->generateUrl('admnistrationutilisateurnew'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+    
+    private function createEditFormUtilisateur(Utilisateur $entity)
+    {
+        $form = $this->createForm(new UtilisateurType(), $entity, array(
+            'action' => $this->generateUrl('administrationutilisateuredit', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    
+    private function createDeleteFormUtilisateur($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('utilisateur_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
