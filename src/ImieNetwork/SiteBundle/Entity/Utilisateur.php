aba57753064041b0ae4b4dba8944e79b826d68cb
Coyote <coyotealone@hotmail.fr>
2014-06-23 16:36:23 +0200

    Modification gitignore Mise à jour Entity Utilisateur

--- src/ImieNetwork/SiteBundle/Entity/Utilisateur.php
+++ src/ImieNetwork/SiteBundle/Entity/Utilisateur.php
@@ -6,358 +6,64 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Utilisateur
- *@ORM\Entity(repositoryClass="ImieNetwork\SiteBundle\Repository\UtilisateurRepository")
+ * @ORM\Entity(repositoryClass="ImieNetwork\SiteBundle\Repository\UtilisateurRepository")
+ * @ORM\Table()
  */class  Utilisateur
 {
     /**
      * @var integer
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $nom;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $prenom;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $adresse;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $telephone;
 
     /**
-     * @var integer
-     */
-    private $statut;
-
-    /**
-     * @var string
-     */
-    private $login;
-
-    /**
-     * @var string
-     */
-    private $pass;
-
-    /**
-     * @var string
-     */
-    private $email;
-
-    /**
      * @var \DateTime
+     * @ORM\Column(type="datetimetz", nullable=false)
      */
     private $datecreation;
 
     /**
      * @var \DateTime
+     * @ORM\Column(type="datetimetz", nullable=false)
      */
     private $datemodification;
 
     /**
      * @var string
+     * @ORM\Column(type="string")
      */
     private $langue;
 
     /**
      * @var \ImieNetwork\SiteBundle\Entity\Ville
+     * @ORM\ManyToOne(targetEntity="Ville")
      */
-    private $idville;
+    private $ville;
 
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set nom
-     *
-     * @param string $nom
-     * @return Utilisateur
-     */
-    public function setNom($nom)
-    {
-        $this->nom = $nom;
-
-        return $this;
-    }
-
-    /**
-     * Get nom
-     *
-     * @return string 
-     */
-    public function getNom()
-    {
-        return $this->nom;
-    }
-
-    /**
-     * Set prenom
-     *
-     * @param string $prenom
-     * @return Utilisateur
-     */
-    public function setPrenom($prenom)
-    {
-        $this->prenom = $prenom;
-
-        return $this;
-    }
-
-    /**
-     * Get prenom
-     *
-     * @return string 
-     */
-    public function getPrenom()
-    {
-        return $this->prenom;
-    }
-
-    /**
-     * Set adresse
-     *
-     * @param string $adresse
-     * @return Utilisateur
-     */
-    public function setAdresse($adresse)
-    {
-        $this->adresse = $adresse;
-
-        return $this;
-    }
-
-    /**
-     * Get adresse
-     *
-     * @return string 
-     */
-    public function getAdresse()
-    {
-        return $this->adresse;
-    }
-
-    /**
-     * Set telephone
-     *
-     * @param string $telephone
-     * @return Utilisateur
-     */
-    public function setTelephone($telephone)
-    {
-        $this->telephone = $telephone;
-
-        return $this;
-    }
-
-    /**
-     * Get telephone
-     *
-     * @return string 
-     */
-    public function getTelephone()
-    {
-        return $this->telephone;
-    }
-
-    /**
-     * Set statut
-     *
-     * @param integer $statut
-     * @return Utilisateur
-     */
-    public function setStatut($statut)
-    {
-        $this->statut = $statut;
-
-        return $this;
-    }
-
-    /**
-     * Get statut
-     *
-     * @return integer 
-     */
-    public function getStatut()
-    {
-        return $this->statut;
-    }
-
-    /**
-     * Set login
-     *
-     * @param string $login
-     * @return Utilisateur
-     */
-    public function setLogin($login)
-    {
-        $this->login = $login;
-
-        return $this;
-    }
-
-    /**
-     * Get login
-     *
-     * @return string 
-     */
-    public function getLogin()
-    {
-        return $this->login;
-    }
-
-    /**
-     * Set pass
-     *
-     * @param string $pass
-     * @return Utilisateur
-     */
-    public function setPass($pass)
-    {
-        $this->pass = $pass;
-
-        return $this;
-    }
-
-    /**
-     * Get pass
-     *
-     * @return string 
-     */
-    public function getPass()
-    {
-        return $this->pass;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     * @return Utilisateur
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string 
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set datecreation
-     *
-     * @param \DateTime $datecreation
-     * @return Utilisateur
-     */
-    public function setDatecreation($datecreation)
-    {
-        $this->datecreation = $datecreation;
-
-        return $this;
-    }
-
-    /**
-     * Get datecreation
-     *
-     * @return \DateTime 
-     */
-    public function getDatecreation()
-    {
-        return $this->datecreation;
-    }
-
-    /**
-     * Set datemodification
-     *
-     * @param \DateTime $datemodification
-     * @return Utilisateur
-     */
-    public function setDatemodification($datemodification)
-    {
-        $this->datemodification = $datemodification;
-
-        return $this;
-    }
-
-    /**
-     * Get datemodification
-     *
-     * @return \DateTime 
-     */
-    public function getDatemodification()
-    {
-        return $this->datemodification;
-    }
-
-    /**
-     * Set langue
-     *
-     * @param string $langue
-     * @return Utilisateur
-     */
-    public function setLangue($langue)
-    {
-        $this->langue = $langue;
-
-        return $this;
-    }
-
-    /**
-     * Get langue
-     *
-     * @return string 
-     */
-    public function getLangue()
-    {
-        return $this->langue;
-    }
-
-    /**
-     * Set idville
-     *
-     * @param \ImieNetwork\SiteBundle\Entity\Ville $idville
-     * @return Utilisateur
-     */
-    public function setIdville(\ImieNetwork\SiteBundle\Entity\Ville $idville = null)
-    {
-        $this->idville = $idville;
-
-        return $this;
-    }
-
-    /**
-     * Get idville
-     *
-     * @return \ImieNetwork\SiteBundle\Entity\Ville 
-     */
-    public function getIdville()
-    {
-        return $this->idville;
-    }
 }
