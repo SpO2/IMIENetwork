9d2875515e96b041d88cad556146f77d164d3dc6
unknown <quentinandrieumail@gmail.com>
2014-06-25 19:09:38 +0200

    Modification liste offre

--- src/ImieNetwork/SiteBundle/Controller/Entreprise/OffreController.php
+++ src/ImieNetwork/SiteBundle/Controller/Entreprise/OffreController.php
@@ -7,16 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class OffreController extends Controller {
 
     public function indexAction() {
-        $em = $this->getDoctrine()->getManager();
-        $utilisateur = $this->getUser();
-
-        $offreNonActif = $em->getRepository('ImieNetworkSiteBundle:OffreRepository')->offresNonActivesParUtilisateur($utilisateur);
-        $offreActif = $em->getRepository('ImieNetworkSiteBundle:OffreRepository')->offresActivesParUtilisateur($utilisateur);
-
-        return $this->render('ImieNetworkSiteBundle:Entreprise:mesOffre.html.twig', array(
-                    'offreNonActif' => $offreNonActif,
-                    'offreActif' => $offreActif,
-        ));
+        return $this->render('@Entreprise/Offre/index.html.twig');
     }
 
 }
