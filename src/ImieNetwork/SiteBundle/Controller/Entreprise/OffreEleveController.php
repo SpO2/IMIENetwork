7bd36d2d287b130325bea73729f63e201b7d9519
unknown <quentinandrieumail@gmail.com>
2014-07-25 16:19:07 +0200

    Search eleve entreprise

--- src/ImieNetwork/SiteBundle/Controller/Entreprise/OffreEleveController.php
+++ src/ImieNetwork/SiteBundle/Controller/Entreprise/OffreEleveController.php
@@ -9,7 +9,13 @@ class OffreEleveController extends Controller {
     public function indexAction() {
         $em = $this->getDoctrine()->getManager();
         $limit = $this->container->getParameter("preview_default");
-
+        $request  = $this->getRequest();
+        
+        $idCompetences = $request->request->get('idCompetence');
+        $idVille = $request->request->get('idVille');
+        print_r($idVille) ;
+        print_r($idCompetences);
+        
         $utilisateurs = $em->getRepository('ImieNetworkSiteBundle:Utilisateur')->findAll($limit);
 
         return $this->render('@Entreprise/OffreEleve/index.html.twig', array(
@@ -18,5 +24,16 @@ class OffreEleveController extends Controller {
     }
     public function showAction() {
         return $this->render('@Entreprise/OffreEleve/show.html.twig');
+    }
+    
+    public function searchAction() {
+        $em = $this->getDoctrine()->getManager();
+        $competences = $em->getRepository('ImieNetworkSiteBundle:Competence')->findAll();
+        $villes = $em->getRepository('ImieNetworkSiteBundle:Ville')->findAll();
+        
+        return $this->render('@Entreprise/OffreEleve/search.html.twig', array(
+                    'competences' => $competences,
+                    'villes' => $villes,
+        ));
     }
 }
